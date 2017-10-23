966f2e32363a16b262dabfca24bb6ec812a29667
RuthAngus <ruth.angus@astro.ox.ac.uk>
2017-10-23 11:52:07 -0400

    making better

--- data/processed/angus/read_fits.py
+++ data/processed/angus/read_fits.py
@@ -1,5 +1,30 @@
+# Read the header of a spectrum fits file. This is mainly for checking the
+# object name.
+
 from astropy.io import fits
-hdulist = fits.open("1d_n1.0131.fit")
+import sys
+
+assert len(sys.argv) > 2, "You need to tell me which night and object " \
+    "number to look at!"
+
+night = sys.argv[1]
+z = 4
+if night == "3":
+    z = 3
+obs_number = str(sys.argv[2]).zfill(z)
+
+hdulist = fits.open("n{0}/1d_n{0}.{1}.fit".format(night, obs_number))
 hdulist.close()
-print(hdulist[0].header)
-print(hdulist[0].header["OBJECT"])
+
+print("Object name = ", hdulist[0].header["OBJECT"])
+
+if input("Would you like to rename the header object? y/n ") == "y":
+    new_name = input("Type the name you'd like: ")
+    proceed2 = input("Replace header name with {}, are you sure? y/n "
+                        .format(new_name))
+    if proceed2 == "y":
+        fits.setval("n{0}/1d_n{0}.{1}.fit".format(night, obs_number),
+                    "OBJECT", value=new_name)
+
+    hdulist = fits.open("n{0}/1d_n{0}.{1}.fit".format(night, obs_number))
+    print("New object name = ", hdulist[0].header["OBJECT"])
