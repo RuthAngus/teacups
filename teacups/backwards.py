832c87698b9b7fa1d1429489cee5527322d066cf
RuthAngus <ruth.angus@astro.ox.ac.uk>
2018-01-11 12:00:43 -0500

    unique tups

--- teacups/backwards.py
+++ teacups/backwards.py
@@ -6,30 +6,80 @@ from astropy.table import Table
 import numpy as np
 import pandas as pd
 
-# Load the observed stars
-observed = pd.read_csv("../data/targets-small-sep.csv")
-print(np.shape(observed), "observed stars")
+# # Load the observed stars
+# observed = pd.read_csv("../data/targets-small-sep.csv")
+# print(np.shape(observed), "observed stars")
 
-# Load the tgas data (just to get the row inds)
-table = Table.read('stacked_tgas.fits')
-all_tgas = table.to_pandas()
+# # Load the tgas data (just to get source id from the row inds)
+# table = Table.read('stacked_tgas.fits')
+# all_tgas = table.to_pandas()
 
-# Load Semyeong's data file. These are ALL the pairs.
-t2 = Table.read('pairindices_cp1.fits')
-pair_df = t2.to_pandas()
-print(np.shape(pair_df), "all pairs")
+# # Load Semyeong's data file. These are ALL the pairs.
+# t2 = Table.read('pairindices_cp1.fits')
+# pair_df = t2.to_pandas()
+# print(np.shape(pair_df), "all pairs")
 
-# Add group_id and source_id columns to semyeong's file.
-pair_df["group_id"] = range(len(pair_df.star1.values))
-pair_df["source_id1"] = all_tgas.source_id.values[pair_df.star1]
-pair_df["source_id2"] = all_tgas.source_id.values[pair_df.star2]
+# # Add pair_id and source_id columns to semyeong's file.
+# pair_df["pair_id"] = range(len(pair_df.star1.values))
+# pair_df["source_id1"] = all_tgas.source_id.values[pair_df.star1]
+# pair_df["source_id2"] = all_tgas.source_id.values[pair_df.star2]
 
-observed_with_group_id1 = pd.merge(observed, pair_df, left_on="source_id",
-                                   right_on="source_id1", how="inner")
-observed_with_group_id = pd.merge(observed_with_group_id1, observed,
-                                  left_on="source_id2",
-                                  right_on="source_id", how="outer",
-                                  suffixes=("_star1", "_star2"))
+# # Merge 1st star in pairs with observed data on source_id to add group ids.
+# # This produces a smaller df with info for just the first stars.
+# # observed_with_pair_id1 = pd.merge(observed, pair_df, left_on="source_id",
+# #                                    right_on="source_id1", how="inner")
+# observed_with_pair_id1 = pd.merge(pair_df, observed, right_on="source_id",
+#                                    left_on="source_id1", how="inner")
 
-print(np.shape(observed_with_group_id), "all observed stars")
-print(observed_with_group_id.keys())
+# # Now do the same for the second stars in the pairs.
+# # observed_with_pair_id2 = pd.merge(observed, pair_df, left_on="source_id",
+# #                                    right_on="source_id2", how="inner")
+# observed_with_pair_id2 = pd.merge(pair_df, observed, right_on="source_id",
+#                                    left_on="source_id2", how="inner")
+
+# # Concatenate these two dfs.
+# joint = pd.concat((observed_with_pair_id1, observed_with_pair_id2))
+
+# joint.to_csv("joint.csv")
+joint = pd.read_csv("joint.csv")
+
+star1 = joint.source_id1.values
+star2 = joint.source_id2.values
+pair_id = joint.pair_id.values
+
+print(len(np.unique(np.concatenate((star1, star2)))), "stars observed")
+
+# Now find repeated pairs of source_ids, either way around.
+tups_fwd = list(zip(joint.source_id1.values, joint.source_id2.values))
+tups_bkwd = list(zip(joint.source_id2.values, joint.source_id1.values))
+for i in tups_bkwd:
+    tups_fwd.append(i)
+tups = tups_fwd
+seen = set()
+result = []
+pair_id_no_dups = []
+for i, tup in enumerate(tups):
+    if tup not in seen and tup[::-1] not in seen and tup[0] != tup[1]:
+         seen.add(tup)
+         result.append(tup)
+         pair_id_no_dups.append(pair_id[i])
+
+def find_new_stars(star, star_array):
+    """
+    Given a star id, find all the pairs to that star in star_array.
+    """
+    m1 = star == star_array[:, 0]
+    m2 = star == star_array[:, 1]
+    if len(star_array[:, 0][m1]):
+        print(star_array[:, 0][m1], star_array[:, 1][m1])
+        print(star_array[:, 1][m2], star_array[:, 0][m2])
+    return star_array[:, 1][m1], star_array[:, 0][m2]
+
+# Now assign group ids. To start with, each pair in tups is a group. If any
+# star is repeated elsewhere, that star's pair is added to the group.
+def find_group(i, star_array):
+    print(tups[i][0], tups[i][0], "\n")
+    find_new_stars(tups[i][0], star_array)
+
+# for i in range(1):
+#     find_group(i, unique_list[inds])
